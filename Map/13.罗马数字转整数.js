6d89c0cc1a679cea2babacaa9471022328241c3a
fxyy1 <1176816800@qq.com>
2021-01-21 00:23:00 +0800

    0120 添加注释

--- Map/13.罗马数字转整数.js
+++ Map/13.罗马数字转整数.js
@@ -30,6 +30,7 @@ var romanToInt = function (s) {
         // i + 1 不会越界，s[i]的值小于后一位
         // 判断原因来源于罗马数字 4 和 9 的规律
         // 以及罗马数字中小的数字在大的数字的右边
+        // 符合：当前位，变减法；不符合：当前位，加法
         if (i < s.length - 1 && temp < map[s[i + 1]]) {
             // IV = -I(-1) + V(5)，以此类推
             // 不要漏掉题目的任何信息
@@ -40,17 +41,25 @@ var romanToInt = function (s) {
     }
     // return res;
 
-    // 思路二：列出所有特殊情况，额外处理
+    // 思路二：列出所有特殊情况，额外处理，使用累加进行计算
     let specialMap = { IV: 4, IX: 9, XL: 40, XC: 90, CD: 400, CM: 900 };
-    return s
-        .split('')
-        .reduce(
-            (prev, curr, index, arr) =>
-                ((arr[index + 1] &&
-                    specialMap[curr + arr[index + 1]] &&
-                    -map[curr]) ||
-                    map[curr]) + prev,
-            0
-        );
+    return s.split('').reduce(
+        // 接受 4 个参数：
+        // 1. Accumulator(acc)(累计器) ；
+        // 2. Current Value(cur)(当前值) ；
+        // 3. Current Index(idx)(当前索引) ；
+        // 4. Source Array (arr) (源数组)；
+        (acc, cur, idx, arr) => {
+            let temp = 0;
+            // 未越界，且存在特殊情况
+            if (arr[idx + 1] && specialMap[cur + arr[idx + 1]]) {
+                temp -= map[cur];
+            } else {
+                temp += map[cur];
+            }
+            return temp + acc;
+        },
+        0 // 初始值为 0
+    );
 };
 // @lc code=end
